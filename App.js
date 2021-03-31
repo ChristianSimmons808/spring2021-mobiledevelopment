76d4641afe276add399da7b3cd060d1d4c3a8114
ChristianSimmons808 <77814745+ChristianSimmons808@users.noreply.github.com>
2021-03-30 22:52:34 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Card, Button} from 'react-native-elements';
+import { Card } from 'react-native-elements';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+const [Sport, setSport] = useState();
+const [votes, setVotes] = useState(false);
+
+function increaseFootballVote(value){
+  setSport('Football');
+  setVotes(true);
+}
+
+function increaseBasketballVote(value){
+  setSport('Basketball');
+  setVotes(true);
+}
+
+function increaseBaseballVote(value){
+  setSport('Baseball');
+  setVotes(true);
+}
+
+
   return (
     <View style={styles.container}>
-<Card>
-<Card.Title>Christian Simmons</Card.Title>
-<Text>Pizza</Text>
-<Card.Divider/>
-<Text>Steak</Text>
-<Button title="Submit" />
+    { votes ? (
+      <View style={styles.container}>
+      <Text>You voted for {Sport}!</Text>
+    </View>
+) : (
+  <View>
+  <Text style={styles.headerText}>Vote for your favorite!</Text>
+  <Card>
+   <Button title="Football" onPress={(value) => (increaseFootballVote('Football'))}/>
+   </Card>
+   <Card.Divider/>
+   <Card>
+   <Button title="Basketball" onPress={(value) => (increaseBasketballVote('Basketball'))}/>
 </Card>
-  </View>
-  );
+<Card.Divider/>
+<Card>
+<Button title="Baseball" onPress={(value) => (increaseBaseballVote('Baseball'))}/>
+</Card>
+<Card.Divider/>
+</View>
+)
+}
+</View>
+
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-});
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  }})
